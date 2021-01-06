996de73e5798f801c5d4cd0aba261216cf12ac79
Michael <mihmih260@gmail.com>
2021-01-06 18:03:00 +0200

    This is my labs

---
+++ Lessons/Lab3.py
@@ -0,0 +1,18 @@
+
+'''
+Create 2 variables : string of your full name, another string of your mail.
+Create variable of your age (integer)
+print all of them to the screen
+then Print your name from the end to the beginning and print it only with your age*3
+then check if your name is stored inside this full string:
+"idan ben dudu yuval shimon yael gal adam shahar yana"
+'''
+
+name="Michael"
+age=34
+mail="mica210@walla.com"
+
+print("My name is: " + name + "\nMy age is: " + str(age) + "\nMy mail is: " + mail)
+print("\nMy reverse name is: " + name[::-1] + "\nMy multiple age is: " + str(age*3))
+print("Michael" in "idan ben dudu yuval kobi shimon gal yael")
+
